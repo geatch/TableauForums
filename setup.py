5cc9f32bf9ed0ddb2e099b285420a98a182c45f4
geatch <65085879+geatch@users.noreply.github.com>
2025-01-26 13:18:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="TableauForums",  # Name of the package
-    version="0.3.1",  # Package version
+    version="0.3.2",  # Package version
     author="Chris Geatch",  # Author's name
     author_email="chris@geatch.com",  # Author's email
     description="For logging the number of points users have on the Tableau Community Forums",  # Short description
     long_description=open("README.md").read(),  # Long description from README
     long_description_content_type="text/markdown",  # Content type of the long description
     url="https://github.com/geatch/TableauForums.git",  # URL to the project's repository
-    packages=find_packages(exclude=["credentials"]),  # Automatically discover packages
+    # packages=find_packages(exclude=["credentials"]),  # Automatically discover packages
+    packages=["TableauForums"]),
+    
     classifiers=[
         "Programming Language :: Python :: 3",  # Target Python version
         "License :: OSI Approved :: MIT License",  # License
